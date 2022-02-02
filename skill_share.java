b257052eb98132da489cf331499c5f368da7b62d
Stanley_Woo <stanleywoo450401@gmail.com>
2022-02-01 19:00:40 -0800

    added a failed test

--- skill_share.java
+++ skill_share.java
@@ -1,1 +1,5 @@
-
+public class skill_share {
+    public static int stanley(){
+        return 50 + 22; //should return 72
+    }
+}
---
+++ skill_share_test.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class skill_share_test {
+    @Test
+    public void testStanley() {
+        assertEquals(2, skill_share.stanley());
+    }
+}
